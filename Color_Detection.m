69c58834fb43aba82f15175785626aff6bc15b0d
Christopher Caperlac <157346675+ChristopherCaperlac@users.noreply.github.com>
2024-03-27 11:09:53 -0700

    Add files via upload

--- Color_Detection.m
+++ Color_Detection.m
@@ -1,35 +1,40 @@
-brick = ConnectBrick('GRANNYVANNY');
+%brick = ConnectBrick('GRANNYVANNY');
 %DisconnectBrick(brick);
 %brick.drawTest();
 %brick.GetBattLevel;
-brick.SetColorMode(3, 2);
+%brick.SetColorMode(3, 2);
 while (true)
+    pause(0.1);
     clc;
     brick.MoveMotor('BC', 50);
     disp(brick.ColorCode(3));
     switch brick.ColorCode(3)
         case 5
-            brick.beep();
             brick.StopAllMotors();
+            brick.WaitForMotor('BC');
+            brick.beep();
             pause(1);
         case 2
-            brick.beep();
             brick.StopAllMotors();
+            brick.WaitForMotor('BC');
+            brick.beep();
             pause(1);
             brick.beep();
             pause(1);
         case 3
-            brick.beep();
             brick.StopAllMotors();
-            pause(1);
+            brick.WaitForMotor('BC');
             brick.beep();
             pause(1);
             brick.beep();
             pause(1);
-        case 1
+            brick.beep();
+            pause(1);
+        case 6
+            brick.StopAllMotors();
+            brick.WaitForMotor('BC');
             brick.playTone(50, 440, 500);
             disp("Stopping Now");
-            brick.StopAllMotors();
             break;
         otherwise
             disp("Color is not coded");
