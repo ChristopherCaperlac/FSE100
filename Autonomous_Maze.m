a2bf5627fb0da198ca95d33aeafd5620a08e7d7b
Christopher Caperlac <157346675+ChristopherCaperlac@users.noreply.github.com>
2024-04-09 12:49:59 -0700

    Update Manual Control and Autonomous Maze
    'R' now moves the grabbing mechanism up and 'F' now moves the grabbing mechanism down. Autonomous Maze Program is updated to its latest version right after 4/8/24.

--- Autonomous_Maze.m
+++ Autonomous_Maze.m
@@ -7,25 +7,39 @@
 while (true)
     pause(0.1);
     clc;
-    brick.MoveMotor('BC', 50);
     color = brick.ColorCode(3);
     disp(color);
+    distance = brick.UltrasonicDist(4);
+    disp(distance);
+    isPressed = brick.TouchPressed(1);
 
-    if color == 1
+    if color == 4
         brick.StopAllMotors();
         brick.WaitForMotor('BC');
-        brick.playTone(50, 440, 500);
+        brick.playTone(10, 440, 500);
         disp("Stopping Now");
         break;
     end
 
-    distance = brick.UltrasonicDist(4);
-    disp(distance);
-
-    if distance <= 20
-        brick.StopAllMotors();
-        brick.MoveMotorAngleRel('B', -50, 300);
-        brick.WaitForMotor('B');
+    if distance > 30
+        %variance = distance - 30;
+        brick.MoveMotor('B', 30);
+        brick.MoveMotor('C', -10);
+        pause(0.1);
     end
 
+    if distance < 20
+        %variance = distance - 20;
+        brick.MoveMotor('C', 30);
+        brick.MoveMotor('B', -10);
+        pause(0.1);
+    end
+
+    brick.MoveMotor('BC', 50);
+
+    if isPressed
+       brick.MoveMotor('BC', -100);
+       pause(0.1);
+       brick.MoveMotor('C', 100);
+    end
 end
--- EV3_Program.m
+++ EV3_Program.m
@@ -1,13 +1,17 @@
 %brick = ConnectBrick('GRANNYVANNY');
 %DisconnectBrick(brick);
 %brick.drawTest();
-%brick.GetBattLevel
+%brick.GetBattLevel;
 
 global key
 InitKeyboard();
 while 1
     pause(0.1);
     clc;
+    color = brick.ColorCode(3);
+    disp(color);
+    distance = brick.UltrasonicDist(4);
+    disp(distance);
     switch key
         case 'w'
             disp('Up Pressed!');
@@ -24,6 +28,14 @@ while 1
         case 'd'
             disp('Right Pressed!');
             brick.MoveMotorAngleRel('B', 25, 180);
+
+        case 'r'
+            disp('Go Up');
+            brick.MoveMotorAngleRel('D', 30, 5);
+
+        case 'f'
+            disp('Go Down');
+            brick.MoveMotorAngleRel('D', -30, 5);
 
         case '0'
             disp('No Key Pressed!');
